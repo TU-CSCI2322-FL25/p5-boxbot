aba5bedd1e42ff1df1ddc5f0ec71d9ef4825ce39
Svetlanist1917 <svetlanist17@gmail.com>
2025-11-07 13:04:09 -0600

    types yay

---
+++ main.hs
@@ -0,0 +1,18 @@
+-- Type Classes
+
+type Point = (Int, Int)
+data Direction = Right | Down
+data Player = X | O
+type Edge = (Point, Direction)
+type Move = Edge
+type Box = (Point, Player)
+type Turn = Player
+data Winner = Maybe Player -- idk
+type Game = ([Edge], Turn, [Box], Int) -- int is a variable square size of the board
+
+
+-- index is 1 thru Size
+-- function: lookup
+-- function: change the players turn
+-- function: (probably list comp) (or a fold idk) Scoreboard system would be defined as a function of scores
+-- function: have a list of all moves, and then immediately subtract the move that any player makes
