fd1a44d52c53694b59241ab96acda15dd1762cee
David Gerber <dGerber@trinity.edu>
2025-11-09 13:59:12 -0600

    Implemented legalMoves

--- main.hs
+++ main.hs
@@ -1,14 +1,23 @@
+import Data.List
+
 -- Type Classes
 
 type Point = (Int, Int)
-data Direction = Right | Down
+data Direction = DirRight | DirDown deriving (Eq, Show)
 data Player = X | O
 type Edge = (Point, Direction)
 type Move = Edge
 type Box = (Point, Player)
 type Turn = Player
-data Winner = Maybe Player -- idk
+type Winner = Maybe Player -- idk
 type Game = ([Edge], Turn, [Box], Int) -- int is a variable square size of the board
+
+legalMoves :: Game -> [Move]
+legalMoves (madeEdges, _, _, size) = allEdges \\ madeEdges
+        where
+                allEdges = rightEdges ++ downEdges
+                rightEdges = [((x,y), DirRight) | x <- [1.. size - 1], y <- [1.. size]]
+                downEdges = [((x,y), DirDown) | x <- [1.. size], y <- [1.. size - 1]]
 
 
 -- index is 1 thru Size
