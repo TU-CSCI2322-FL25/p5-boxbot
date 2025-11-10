f43d5ba0fe0e75390a1765b57e9be4cefd0075ac
Jason Rangel <jrangel2@trinity.edu>
2025-11-10 13:21:02 -0600

    update

--- main.hs
+++ main.hs
@@ -16,3 +16,14 @@ type Game = ([Edge], Turn, [Box], Int) -- int is a variable square size of the b
 -- function: change the players turn
 -- function: (probably list comp) (or a fold idk) Scoreboard system would be defined as a function of scores
 -- function: have a list of all moves, and then immediately subtract the move that any player makes
+
+moveExists :: Edge -> [Edge] -> Bool
+moveExists m ex = m `elem` ex
+
+makeMove :: Game -> Move -> Maybe Game
+makeMove (edges, turn, boxes, size) move =
+  if moveExists move edges
+     then Nothing -- bascially the move isn't allowed idk how else to show it
+  else
+     let newEdges = move : edges
+     in Just (newEdges, turn, boxes, size)
